fe27bf9f94298e276c7fe85c13dd5d5ade841234
DeMarcus Cousins IV <asiynbusiness@gmail.com>
2026-01-14 18:59:30 +0100

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,25 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-        <h1 className={styles.title}>KH10 elen har gått</h1>
+      <h1 className={styles.title}>KH10 elen har gått</h1>
 
-        <div className={styles.loginContainer}>
-          <label htmlFor="uname">Användarnamn</label>
-          <input type="text" placeholder="Skriv användarnamn" name="uname" required/>
+      <div className={styles.loginContainer}>
+        <label htmlFor="user">Användarnamn</label>
+        <input
+          type="text"
+          placeholder="Skriv användarnamn"
+          name="user"
+          required
+        />
 
-          <label htmlFor="pwd">Lösenord</label>
-          <input type="password" placeholder="Skriv lösenord" name="pwd"  required />
-        </div>
+        <label htmlFor="pwd">Lösenord</label>
+        <input
+          type="password"
+          placeholder="Skriv lösenord"
+          name="pwd"
+          required
+        />
+      </div>
     </div>
   );
 }
